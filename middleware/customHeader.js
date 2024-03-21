d5030d76781a6f1c26e49cdfcc80c9904405c19b
roxsyVq <roxsy.dj@gmail.com>
2024-03-21 09:23:07 -0500

    add middleware custom

---
+++ middleware/customHeader.js
@@ -0,0 +1,15 @@
+const customHeader = (req, res, next) => {
+    try {
+        const apiKey = req.headers['api_key'];
+        if (apiKey === "rous-02") {
+            next
+        } else {
+            res.status(403);
+            res.send({ error: "API_KEY_INVALIDA" });
+        }
+    } catch (error) {
+        res.status(403);
+        res.send({ error: "ALGO-OCURRIO_EN_EL_PROCESO" });
+    }
+}
+module.exports = {customHeader}
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {validatorCreateItem} = require('../validators/tracks');
+const {customHeader} = require('../middleware/customHeader');
 const {getItems, getItem, createItem} = require('../controllers/tracks');
 
 
 
 router.get('/', getItems) ;
 
-router.post("/", validatorCreateItem,createItem);
+router.post("/", validatorCreateItem, customHeader, createItem);
 
 
 
